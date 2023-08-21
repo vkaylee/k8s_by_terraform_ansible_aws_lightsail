980a6f27d6359bb84989069b4febe227e8af1dbb
Lee Tuan <me@vlee.dev>
2023-08-21 13:52:51 +0700

    Improvement: use variable for port_info block in aws_lightsail_instance_public_ports resource

--- network.resource.tf
+++ network.resource.tf
@@ -1,86 +1,51 @@
 resource "aws_lightsail_instance_public_ports" "master" {
-  count = length(aws_lightsail_instance.master)
-  instance_name = aws_lightsail_instance.master[count.index].name
-  port_info {
-    protocol = "tcp"
-    from_port = 22
-    to_port = 22
+  for_each = { for index, instance in aws_lightsail_instance.master: index => instance }
+  instance_name = each.value.name
+  dynamic "port_info" {
+    for_each = var.master_ports
+    content {
+      from_port = port_info.value.port
+      to_port   = port_info.value.port
+      protocol  = port_info.value.protocol
+    }
   }
-  # TCP	Inbound	2379-2380	etcd server client API	kube-apiserver, etcd
-  port_info {
-    protocol = "tcp"
-    from_port = 2379
-    to_port = 2380
-  }
-  port_info {
-    protocol = "tcp"
-    from_port = 6443
-    to_port = 6443
-  }
-  # port_info {
-  #   protocol = "tcp"
-  #   from_port = 10250
-  #   to_port = 10252
-  # }
-  # port_info {
-  #   protocol = "tcp"
-  #   from_port = 10255
-  #   to_port = 10255
-  # }
 }
 
 resource "aws_lightsail_instance_public_ports" "master_lb" {
-  count = length(aws_lightsail_instance.master_lb)
-  instance_name = aws_lightsail_instance.master_lb[count.index].name
-  port_info {
-    protocol = "tcp"
-    from_port = 22
-    to_port = 22
-  }
-  port_info {
-    protocol = "tcp"
-    from_port = 6443
-    to_port = 6443
-  }
-  # Port for haproxy stats
-  port_info {
-    protocol = "tcp"
-    from_port = 9000
-    to_port = 9000
+  for_each = { for index, instance in aws_lightsail_instance.master_lb: index => instance }
+  instance_name = each.value.name
+  dynamic "port_info" {
+    for_each = var.master_lb_ports
+    content {
+      from_port = port_info.value.port
+      to_port   = port_info.value.port
+      protocol  = port_info.value.protocol
+    }
   }
 }
 
 resource "aws_lightsail_instance_public_ports" "worker" {
-  count = length(aws_lightsail_instance.worker)
-  instance_name = aws_lightsail_instance.worker[count.index].name
-  port_info {
-    protocol = "tcp"
-    from_port = 22
-    to_port = 22
+  for_each = { for index, instance in aws_lightsail_instance.worker: index => instance }
+  instance_name = each.value.name
+  dynamic "port_info" {
+    for_each = var.worker_ports
+    content {
+      from_port = port_info.value.port
+      to_port   = port_info.value.port
+      protocol  = port_info.value.protocol
+    }
   }
-  # port_info {
-  #   protocol = "tcp"
-  #   from_port = 80
-  #   to_port = 80
-  # }
-  # port_info {
-  #   protocol = "tcp"
-  #   from_port = 10250
-  #   to_port = 10250
-  # }
 }
 
 resource "aws_lightsail_instance_public_ports" "worker_lb" {
-  count = length(aws_lightsail_instance.worker_lb)
-  instance_name = aws_lightsail_instance.worker_lb[count.index].name
-  port_info {
-    protocol = "tcp"
-    from_port = 22
-    to_port = 22
+  for_each = { for index, instance in aws_lightsail_instance.worker_lb: index => instance }
+  instance_name = each.value.name
+  dynamic "port_info" {
+    for_each = var.worker_lb_ports
+    content {
+      from_port = port_info.value.port
+      to_port   = port_info.value.port
+      protocol  = port_info.value.protocol
+    }
   }
-  # port_info {
-  #   protocol = "tcp"
-  #   from_port = 80
-  #   to_port = 80
-  # }
 }
--- variable.tf
+++ variable.tf
@@ -32,6 +32,21 @@ variable "master_blueprint_id" {
 variable "master_bundle_id" {
   default = "medium_3_0"
 }
+variable master_ports {
+  type = list(object({
+    protocol  = string
+    port      = number
+  }))
+  default = [
+    # SSH port
+    { protocol = "tcp", port = 22},
+    # TCP	Inbound	2379-2380	etcd server client API	kube-apiserver, etcd
+    { protocol = "tcp", port = 2379},
+    { protocol = "tcp", port = 2380},
+    # Kube API server
+    { protocol = "tcp", port = 6443},
+  ]
+}
 
 variable "master_lb_count" {
   default = 1
@@ -42,6 +57,20 @@ variable "master_lb_blueprint_id" {
 }
 variable "master_lb_bundle_id" {
   default = "nano_3_0"
+}
+variable master_lb_ports {
+  type = list(object({
+    protocol  = string
+    port      = number
+  }))
+  default = [
+    # SSH port
+    { protocol = "tcp", port = 22},
+    # Kube API server
+    { protocol = "tcp", port = 6443},
+    # Port for haproxy stats
+    { protocol = "tcp", port = 9000},
+  ]
 }
 
 variable "worker_count" {
@@ -54,6 +83,18 @@ variable "worker_blueprint_id" {
 variable "worker_bundle_id" {
   default = "medium_3_0"
 }
+variable worker_ports {
+  type = list(object({
+    protocol  = string
+    port      = number
+  }))
+  default = [
+    # SSH port
+    { protocol = "tcp", port = 22},
+    { protocol = "tcp", port = 30080},
+    { protocol = "tcp", port = 30443},
+  ]
+}
 
 variable "worker_lb_count" {
   default = 1
@@ -64,6 +105,16 @@ variable "worker_lb_blueprint_id" {
 }
 variable "worker_lb_bundle_id" {
   default = "nano_3_0"
+}
+variable worker_lb_ports {
+  type = list(object({
+    protocol  = string
+    port      = number
+  }))
+  default = [
+    # SSH port
+    { protocol = "tcp", port = 22},
+  ]
 }
 
 variable "user_data" {
