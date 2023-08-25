567faf10032541edf3623fe2d7c1a4047106c54e
Lee Tuan <me@vlee.dev>
2023-08-25 13:16:33 +0700

    Change the version pattern to 1.27.* for kubeadm, kubelet, kubectl to allow hotfixes

--- provider.tf
+++ provider.tf
@@ -16,3 +16,12 @@ provider "aws" {
   access_key = var.AWS_ACCESS_KEY
   secret_key = var.AWS_SECRET_KEY
 }
+
+# Additional provider configuration for us-east-1 coast region; resources can
+# reference this as `aws.us-east-1`.
+provider "aws" {
+  alias  = "us-east-1"
+  region = "us-east-1"
+  access_key = var.AWS_ACCESS_KEY
+  secret_key = var.AWS_SECRET_KEY
+}
